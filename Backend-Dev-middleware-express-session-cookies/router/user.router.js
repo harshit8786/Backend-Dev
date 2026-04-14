e4c3ac4c642c7f5227dd973b75fb1461978485b2
Harshit Singh <153013485+harshit8786@users.noreply.github.com>
2026-04-14 19:30:00 +0530

    Delete Backend-Dev-middleware-express-session-cookies directory

--- Backend-Dev-middleware-express-session-cookies/router/user.router.js
+++
@@ -1,8 +0,0 @@
-import usercontroller from "../controller/user.controller.js"
-import exress from "express"
-let userRouter = exress.Router()
-
-userRouter.post("/signup", usercontroller)
-
- 
-export default  userRouter
