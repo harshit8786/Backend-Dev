76d4cbc275e4a6a74d3164dbf495c31bd6db46bf
Harshit Singh <153013485+harshit8786@users.noreply.github.com>
2026-04-20 10:32:34 +0530

    Update member import and change member name

--- Backend-Dev-nodejs-programming/LibaryManagementSystem/index.js
+++ Backend-Dev-nodejs-programming/LibaryManagementSystem/index.js
@@ -2,7 +2,7 @@
 // book( "Math", "N.P Mishra", 500)
 
 // import member from "./src/member.js";
-// member ("Anuj", "gold")
+// member ("Harshit", "gold")
 
 import BorrowRecord from "./src/BorrowRecord.js";
 BorrowRecord("2026-01-29T09:41:32.774Z", "2026-01-29T09:21:18.677Z", 2)
