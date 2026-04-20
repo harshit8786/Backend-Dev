307a2f3088fababd1942052658c47c3b4fc11bcf
Harshit Singh <153013485+harshit8786@users.noreply.github.com>
2026-04-20 10:44:35 +0530

    ..

--- RESTFUL/index.js
+++ RESTFUL/index.js
@@ -1,11 +1,8 @@
-//dependencies
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
-
-//Scripts
 import addBooks from "./question4_CreateBook.js";
 import updateBook from "./question4_UpdateBook.js";
 import deleteBook from "./question4_deleteBook.js";
@@ -14,7 +11,6 @@ import authorBook from "./question1_authorFiltering.js";
 import yearBook from "./question1_yearFiltering.js";
 import validateYear from "./question2_validateYear.js"
 
-//running all dependencies
 dotenv.config();
 const app = express();
 app.use(express.json())
@@ -22,20 +18,17 @@ app.use(helmet());
 app.use(cors());
 app.use(morgan("dev"));
 
-// testing
 let port = process.env.PORT || 8001
 app.get("/", (req,res)=>{
     res.send("<h1>Hii</h1>");
 });
 
-//running scripts
 app.post("/add",addBooks);
 app.post("/update/:id",updateBook);
 app.post("/delete/:id",deleteBook);
 app.post("/read",readBook);
 app.post("/author",authorBook);
 app.post("/year",validateYear,yearBook);
-//connection
 app.listen(port,()=>{
     console.log("Server Connected on the PORT");
 })
